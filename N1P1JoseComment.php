56e5270a3bc2642468cc71ebc3fa5de813f97596
JoseMontejo12 <94554901+JoseMontejo12@users.noreply.github.com>
2021-11-17 17:38:10 +0100

    practica terminada

---
+++ N1P1JoseComment.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<?php
+echo '<ul>';
+echo '<li>h</li>';
+echo '<li>o</li>';
+echo '<li>l</li>';
+echo '<li>a</li>';
+echo '<li>g</li>';
+echo '<li>e</li>';
+echo '<li>n</li>';
+echo '<li>t</li>';
+echo '<li>e</li>';
+echo '<li>!</li>';
+echo '<li>Nota del doc ?, del profe A y yo ? </li>';
+echo '</ul>';
+echo '<p>En que puedo mejorar, en mi concentracion</p>';
+?>
+
+</body>
+</html>
+
